6493d9048b6cfd8dc3479eae358ce072cdb9ba9e
Gabriel Boff <gabrielchagasboff@gmail.com>
2019-12-10 19:17:21 -0300

    the changes in models

--- DatasetBuild.py
+++ DatasetBuild.py
@@ -1,8 +1,8 @@
-def build_data(port, file_name):
+def build_data(port, file_name, columns_list):
     from ReadSerial import read_serial
     import pandas as pd
     import numpy as np
     data = read_serial(port)
     real_data = np.array(data)
-    df = pd.DataFrame(real_data, index=[ ind for ind, x in enumerate(real_data)] columns='TEMPERATURE HUMIDITY'.split())
+    df = pd.DataFrame(real_data, index=[ ind for ind, x in enumerate(real_data)] columns=columns_list)
     return df.to_csv(file_name, encoding='utf-8', index=False)
--- RandomForestMethod.py
+++ RandomForestMethod.py
@@ -1,4 +1,4 @@
-def build_model_forest(dataset_path, features_list, target, max_leaf_nodes, max_depth=None):
+def build_model_forest(dataset_path, features_list, target, max_leaf_nodes, max_depth=None, n_estimators=None):
     import pandas as pd
     from sklearn.metrics import mean_absolute_error
     from sklearn.model_selection import train_test_split
@@ -12,7 +12,7 @@ def build_model_forest(dataset_path, features_list, target, max_leaf_nodes, max_
 
     train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
-    rf_model1 = RandomForestRegressor(max_depth=max_depth)
+    rf_model1 = RandomForestRegressor(max_depth=max_depth, n_estimators=n_estimators )
     rf_model1.fit(train_X, train_y)
     rf_model_predict1 = rf_model1.predict(val_X)
 
@@ -20,7 +20,7 @@ def build_model_forest(dataset_path, features_list, target, max_leaf_nodes, max_
 
     print('Validation MAE for Random Forest Model: {:,.0f}'.format(rf_val_mae1))
 
-    rf_model2 = RandomForestRegressor(max_depth=max_depth, max_leaf_nodes=max_leaf_nodes)
+    rf_model2 = RandomForestRegressor(max_depth=max_depth, max_leaf_nodes=max_leaf_nodes,  n_estimators=n_estimators )
     rf_model2.fit(train_X, train_y)
     rf_model_predict2 = rf_model2.predict(val_X)
 
