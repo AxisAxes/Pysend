e94e65b91063f8f866c61346975118d67d23ae07
Gabriel Boff <gabrielchagasboff@gmail.com>
2019-12-10 19:54:13 -0300

    the main started

--- DatasetBuild.py
+++ DatasetBuild.py
@@ -4,5 +4,5 @@ def build_data(port, file_name, columns_list):
     import numpy as np
     data = read_serial(port)
     real_data = np.array(data)
-    df = pd.DataFrame(real_data, index=[ ind for ind, x in enumerate(real_data)] columns=columns_list)
+    df = pd.DataFrame(real_data, index=[ ind for ind, x in enumerate(real_data)] , columns=columns_list)
     return df.to_csv(file_name, encoding='utf-8', index=False)
