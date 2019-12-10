bef42663b23d6ca9142f59dcd85dd7070c4192bc
Gabriel Boff <gabrielchagasboff@gmail.com>
2019-12-10 19:57:05 -0300

    the remove score

--- RandomForestMethod.py
+++ RandomForestMethod.py
@@ -1,10 +1,13 @@
+def score_model(model, X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
+    model.fit(X_t, y_t)
+    preds = model.predict(X_v)
+    return mean_absolute_error(y_v, preds)
 
 def build_model_forest(dataset_path, features_list, target, max_leaf_nodes, max_depth=None, n_estimators=None):
     import pandas as pd
     from sklearn.metrics import mean_absolute_error
     from sklearn.model_selection import train_test_split
     from sklearn.ensemble import RandomForestRegressor, plot_tree
-    from ScoreModels import score_model
 
     df = pd.read_csv(dataset_path)
     df.head()
--- ScoreModels.py
+++
@@ -1,4 +0,0 @@
-def score_model(model, X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
-    model.fit(X_t, y_t)
-    preds = model.predict(X_v)
-    return mean_absolute_error(y_v, preds)
