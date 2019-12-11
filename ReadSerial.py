a10640032beb7ee45dd7919543537ec18240ae4e
Gabriel Boff <gabrielchagasboff@gmail.com>
2019-12-11 20:09:48 -0300

    ops

--- ReadSerial.py
+++ ReadSerial.py
@@ -2,7 +2,6 @@ import serial
 import time
 def read_serial():
     print('ENTRY METHOD 1')
-    list_data_rcv = []
     timeout = time.time() + 30  
     print('EXECUTATE')
     ser = serial.Serial('COM7', 9600, timeout=1) 
